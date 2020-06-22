9831a20fdadd57126660a301a526abad6dbb8e64
ArcadiaEmano <mendoza.emano@gmail.com>
2020-06-22 15:54:12 -0600

    Customing Generated HTML Files

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
@@ -9,7 +10,7 @@ module.exports = {
     output: {
         filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
-        publicPath:'dist/'
+        publicPath:''
     },
     mode: 'none',
 
@@ -53,11 +54,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css'
         }),
-        new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: [
-                '**/*',
-                path.join(process.cwd(), 'build/**/*')
-            ]
-        })
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin()
     ]
 };
