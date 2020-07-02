e902845e90554fb86743069b2706b1c108bf39ec
ArcadiaEmano <mendoza.emano@gmail.com>
2020-07-02 16:43:32 -0600

    problema con la configuracion dev

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,7 +11,11 @@ module.exports = {
         publicPath:''
     },
     mode: 'development',
-
+    devServer:{
+        contentBase: path.resolve(__dirname, './dist'),
+        index: 'index.html',
+        port: 9000
+    },
     module:{
         rules:[
            
